4fbc8dda653a4c1b9a9a806de33cf2bccc96f051
Behlül Uçar <ucarbehlul@gmail.com>
2020-03-22 17:53:58 -0700

    Initial commit

---
+++ rumi2miladi.js
@@ -0,0 +1,31 @@
+#!node
+
+var moment = require('moment');
+
+const input = process.argv[2];
+//miladide 29 cekmeyen 1900 yılına özel çözüm
+if (input == '1315-02-29') {
+  console.log('1900-03-13');
+  return
+}
+
+let rumiTarih = moment(input);
+const miladiBasSene = 1840;
+const ayniGunOlmaTarihi = moment('1333-03-01');
+const onUcGunOlmaTarihi = moment('1315-03-01');
+
+//ayni gun olmalarindan onceyse 13 gun cikar
+const adjustedRumi = rumiTarih.clone();
+if (rumiTarih.isBefore(onUcGunOlmaTarihi)) {
+  adjustedRumi.add(12, 'days');
+} else if (rumiTarih.isBefore(ayniGunOlmaTarihi)) {
+  adjustedRumi.add(13, 'days');
+}
+
+console.debug("adjusted rumi", adjustedRumi.format());
+
+const month = adjustedRumi.month()+1 // make it 1-indexed
+const year = miladiBasSene + (adjustedRumi.year()-1256) + (month < 3 ? 1 : 0)
+
+console.log(`${year}-${month}-${adjustedRumi.date()}`);
+
